4b6379a854730833229548183dc77c0f5e22ed96
rust-master <muhammadzaryabrafique@gmail.com>
2024-01-04 17:41:01 +0500

    formatting

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
-// require('isomorphic-fetch'); // or another library of choice.
-var Dropbox = require('dropbox').Dropbox;
+require("dotenv").config();
+var Dropbox = require("dropbox").Dropbox;
 
-console.log("Token:", process.env.TOKEN)
+console.log("Token:", process.env.TOKEN);
 
 var dbx = new Dropbox({ accessToken: process.env.TOKEN });
-dbx.filesListFolder({path: ''})
-  .then(function(response) {
+dbx
+  .filesListFolder({ path: "" })
+  .then(function (response) {
     console.log(response);
   })
-  .catch(function(error) {
+  .catch(function (error) {
     console.log(error);
   });
